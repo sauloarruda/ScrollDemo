40c6f7207060b4c1ded3b4ef565651dedb694f45
Saulo Arruda <sauloarruda@gmail.com>
2012-09-11 21:19:48 -0400

    Implementação do scrollview com 2 situações: banner (slider) e zoom

--- ScrollDemo/SDViewController.m
+++ ScrollDemo/SDViewController.m
@@ -10,20 +10,32 @@
 
 @interface SDViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *zoomImageView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @end
 
 @implementation SDViewController
+@synthesize zoomImageView;
+@synthesize scrollView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.scrollView.contentSize = CGSizeMake(320*3, 120);
 }
 
 - (void)viewDidUnload
 {
+    [self setScrollView:nil];
+    [self setZoomImageView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.zoomImageView;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
